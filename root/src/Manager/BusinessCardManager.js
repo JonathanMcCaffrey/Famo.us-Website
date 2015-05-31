af2f397db14a1cb6ac7d99b95614ef7a7e72d73c
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-05-31 19:29:20 -0400

    Added front and backside to cards

--- root/src/Manager/BusinessCardManager.js
+++ root/src/Manager/BusinessCardManager.js
@@ -5,11 +5,9 @@ var FamousEngine = require('famous/core/FamousEngine');
 
 function BusinessCardManager(node) {
 
-
     this.node = node;
 
     var children = [];
-
 
     this.addChild = function(child) {
         children.push(child);
@@ -17,7 +15,7 @@ function BusinessCardManager(node) {
 
     var deltaTime = 0;
     var direction = 1;
-    var speed = 0.14;
+    var speed = 0.11;
     var refresher = node.addComponent({
         onUpdate: function (time) {
 
@@ -26,11 +24,19 @@ function BusinessCardManager(node) {
             if(deltaTime > (Math.PI / 2)) {
                 deltaTime = (Math.PI / 2);
                 direction = -1;
+
+                for (var index = 0; index < children.length; index++) {
+                    children[index].backSide.node.setScale(0,0,0);
+                }
             }
 
-            if(deltaTime < 0) {
-                deltaTime = 0;
+            if(deltaTime < -(Math.PI / 2)) {
+                deltaTime = -(Math.PI / 2);
                 direction = 1;
+
+                for (var index = 0; index < children.length; index++) {
+                    children[index].backSide.node.setScale(1,1,1);
+                }
             }
 
             for (var index = 0; index < children.length; index++) {
@@ -44,7 +50,6 @@ function BusinessCardManager(node) {
     });
 
     node.requestUpdate(refresher);
-
 
 };
 
--- root/src/UI/Button/BusinessCard.js
+++ root/src/UI/Button/BusinessCard.js
@@ -3,8 +3,30 @@
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
 
+
+function FrontSide(node, imageName, link) {
+    this.node = node;
+
+    this.image = new DOMElement(node, {tagName: 'img'})
+        .setAttribute('src', imageName);
+    this.image.setProperty('cursor', 'pointer');
+
+}
+
+function BackSide(node) {
+    this.node = node;
+
+    this.image = new DOMElement(node, {tagName: 'img'})
+        .setAttribute('src', './images/BusinessCards/card-placeholder.png');
+    this.image.setProperty('cursor', 'pointer');
+
+}
+
 function BusinessCard(node, imageName, link) {
     this.node = node;
+
+    this.frontSide = new FrontSide(node.addChild(), imageName, link);
+    this.backSide = new BackSide(node.addChild());
 
     this.node
         .setSizeMode('absolute', 'absolute', 'absolute')
@@ -12,11 +34,6 @@ function BusinessCard(node, imageName, link) {
         .setAbsoluteSize(350, 200)
         .setMountPoint(0.5, 0.5)
         .setOrigin(0.5, 0.5);
-
-    this.el = new DOMElement(node, {tagName: 'img'})
-        .setAttribute('src', imageName);
-    this.el.setProperty('cursor', 'pointer');
-    this.el.setProperty('zIndex', '2');
 
     this.node.addUIEvent("click");
 
